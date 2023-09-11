b93eaf8a79aa652908ff97e98267d0c5a0e1d6a9
= <=>
2023-09-11 16:39:22 -0700

    Initial Commit

---
+++ DemoCub/DemoCubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCubApp.swift
+//  DemoCub
+//
+//  Created by Kaung Set Lin on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoCubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
